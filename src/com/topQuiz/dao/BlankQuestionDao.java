bf1eecdc188e935c031114622f303f8c6520e242
Zihan <zihanzhang0831@gmail.com>
2020-03-11 15:49:58 -0700

    Add the first and second game

---
+++ src/com/topQuiz/dao/BlankQuestionDao.java
@@ -0,0 +1,43 @@
+package com.topQuiz.dao;
+
+import com.topQuiz.model.BlankQuestion;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * blank question DAO
+ * @author jady
+ *
+ */
+public class BlankQuestionDao {
+
+	/**
+	* add blank question
+	* @param con
+	* @param blankQuestion
+	* @return
+	* @throws Exception
+	*/
+	public int add(Connection con, BlankQuestion blankQuestion) throws Exception{
+	    String sql = "insert into t_blankQuestion values(null,?,?)";
+	    PreparedStatement pstmt = con.prepareStatement(sql);
+	    pstmt.setString(1, blankQuestion.getQuestion());
+	    pstmt.setString(2, blankQuestion.getAnswer());
+	    return pstmt.executeUpdate(); // return affected rows
+	}
+
+	public ResultSet search(Connection con) throws Exception {
+		String sql = "select * from t_blankQuestion order by rand() limit 2";
+		PreparedStatement pstmt = con.prepareStatement(sql);
+		return pstmt.executeQuery();
+	}
+
+
+	
+}
+
+
+
+
