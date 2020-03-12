2f6175bf5f7f48b034c83ca43bf0fcb057d42f25
Jady <jady.zhang05@gmail.com>
2020-03-12 12:49:47 -0700

    Remove button Back To Home

--- src/com/topQuiz/view/ChooseQuesType.java
+++ src/com/topQuiz/view/ChooseQuesType.java
@@ -1,96 +1,83 @@
 package com.topQuiz.view;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+        import javax.swing.*;
+        import java.awt.*;
+        import java.awt.event.ActionEvent;
+        import java.awt.event.ActionListener;
 
 // Java extension packages
 
 public class ChooseQuesType extends JFrame {
-	private JPanel panel1,panel2;
-	private JButton blankQuesBtn, choiceQuesBtn, backToHomeBtn;
+   private JPanel panel1;
+   private JButton blankQuesBtn, choiceQuesBtn;
 
-	public ChooseQuesType(){
-		super( "Choose Question Type" );
+   public ChooseQuesType(){
+      super( "Choose Question Type" );
 
-		// get content pane
-		Container container = getContentPane();
+      // get content pane
+      Container container = getContentPane();
 
-		panel1 = new JPanel();
-		panel1.setLayout(new FlowLayout());
+      panel1 = new JPanel();
+      panel1.setLayout(new FlowLayout());
 
-		// create buttons
-		blankQuesBtn = new JButton("Blank Question");
-		blankQuesBtn.setPreferredSize(new Dimension(150, 150));
-		blankQuesBtn.setFont(new Font("Arial", Font.BOLD, 16));
-		blankQuesBtn.setBackground(Color.WHITE);
-		blankQuesBtn.setForeground(Color.RED);
-		blankQuesBtn.setActionCommand("blankQues");
+      // create buttons
+      blankQuesBtn = new JButton("Blank Question");
+      blankQuesBtn.setPreferredSize(new Dimension(150, 150));
+      blankQuesBtn.setFont(new Font("Arial", Font.BOLD, 16));
+      blankQuesBtn.setBackground(Color.WHITE);
+      blankQuesBtn.setForeground(Color.RED);
+      blankQuesBtn.setActionCommand("blankQues");
 
-		choiceQuesBtn = new JButton("Choice Question");
-		choiceQuesBtn.setPreferredSize(new Dimension(150, 150));
-		choiceQuesBtn.setFont(new Font("Arial", Font.BOLD, 16));
-		choiceQuesBtn.setBackground(Color.WHITE);
-		choiceQuesBtn.setForeground(Color.BLUE);
-		choiceQuesBtn.setActionCommand("choiceQues");
+      choiceQuesBtn = new JButton("Choice Question");
+      choiceQuesBtn.setPreferredSize(new Dimension(150, 150));
+      choiceQuesBtn.setFont(new Font("Arial", Font.BOLD, 16));
+      choiceQuesBtn.setBackground(Color.WHITE);
+      choiceQuesBtn.setForeground(Color.BLUE);
+      choiceQuesBtn.setActionCommand("choiceQues");
 
-		panel1.add(blankQuesBtn);
-		panel1.add(choiceQuesBtn);
+      panel1.add(blankQuesBtn);
+      panel1.add(choiceQuesBtn);
 
-		panel2 = new JPanel();
-		panel2.setLayout(new FlowLayout());
+      // Use an anonymous class as an event handler
+      blankQuesBtn.addActionListener( new ActionListener(){
+         public void actionPerformed(ActionEvent event){
+            dispose();
+            new FillBlankQues().setVisible(true);
+         }
 
-		backToHomeBtn = new JButton("Back To Home");
-		backToHomeBtn.setActionCommand("back to home");
-		backToHomeBtn.setFont(new Font("Arial", Font.BOLD, 15));
+      });
 
-		// Use an anonymous class as an event handler
-		blankQuesBtn.addActionListener( new ActionListener(){
-			public void actionPerformed(ActionEvent event){
-				dispose();
-				new FillBlankQues().setVisible(true);
-			}
+      choiceQuesBtn.addActionListener( new ActionListener(){
+         public void actionPerformed(ActionEvent event){
+            dispose();
+            new FillChoiceQues().setVisible(true);
+         }
 
-		});
+      });
 
-		choiceQuesBtn.addActionListener( new ActionListener(){
-			public void actionPerformed(ActionEvent event){
-				dispose();
-				new FillChoiceQues().setVisible(true);
-			}
+      container.add(panel1,BorderLayout.CENTER);
 
-		});
+      pack();
+      setLocationRelativeTo(null);
+      setVisible( true );
+   }
 
-		backToHomeBtn.addActionListener(new ActionListener() {
-			public void actionPerformed( ActionEvent event ) {
-				dispose();
-				new TopQuizMain().setVisible(true);
-			}
-		});
 
-		panel2.add(backToHomeBtn);
+   public static void createAndShowGUI(){
+      ChooseQuesType app = new ChooseQuesType();
+      app.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE );
+   }
 
-		container.add(panel1,BorderLayout.CENTER);
-		container.add(panel2,BorderLayout.SOUTH);
+   public static void main(String[] args) {
+      //Schedule a job for the event dispatch thread:
+      //creating and showing this application's GUI.
+      SwingUtilities.invokeLater(new Runnable() {
+         public void run() {
+            createAndShowGUI();
+         }
+      });
+   }
 
-		pack();
-		setLocationRelativeTo(null);
-		setVisible( true );
-	}
-
-	public static void createAndShowGUI(){
-		ChooseQuesType app = new ChooseQuesType();
-		app.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE );
-	}
-
-	public static void main(String[] args) {
-		//Schedule a job for the event dispatch thread:
-		//creating and showing this application's GUI.
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				createAndShowGUI();
-			}
-		});
-	}
 }
+
+
